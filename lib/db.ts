55c85894a9972fc9fb28ed770f1945fb002edb5a
Pat <amador.patricia@hotmail.com>
2026-04-20 12:43:45 -0700

    added database connection

--- lib/db.ts
+++ lib/db.ts
@@ -2,6 +2,9 @@ import postgres from "postgres";
 
 const sql = postgres(process.env.DATABASE_URL!, {
   ssl: "require",
+  max: 10,              // maximum connections in pool
+  idle_timeout: 20,     // close idle connections after 20s
+  connect_timeout: 10,  // fail if connection takes over 10s
 });
 
 export default sql;
